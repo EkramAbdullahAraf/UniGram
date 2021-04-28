4d14e4fd84763d190d77736bd8461f1cfd87dc70
MD. Shakil <74317232+EkramAbdullahAraf@users.noreply.github.com>
2021-04-28 16:05:27 +0600

    Updated

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
+
+Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
